9e2431a658bd9360e1e394bb79565eebbb5eb8b5
Regan Karlewicz <regan@econify.com>
2021-09-13 17:54:59 -0700

    [RK] Typescript lint

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,4 +1,4 @@
-import { ILogger, LogLevel } from "./types";
+import { ILogger, LogLevel } from './types';
 
 export default class Logger implements ILogger {
   private loggerObject: ILogger;
@@ -13,29 +13,29 @@ export default class Logger implements ILogger {
     return level <= this.logLevel;
   }
 
-  public setLogLevel(level: LogLevel) {
+  public setLogLevel(level: LogLevel): void {
     this.logLevel = level;
   }
 
-  public debug(message: String) {
+  public debug(message: string): void {
     if (this.shouldLog(LogLevel.DEBUG)) {
       this.loggerObject.debug(message);
     }
   }
 
-  public info(message: String) {
+  public info(message: string): void {
     if (this.shouldLog(LogLevel.INFO)) {
       this.loggerObject.info(message);
     }
   }
 
-  public warn(message: String) {
+  public warn(message: string): void {
     if (this.shouldLog(LogLevel.WARN)) {
       this.loggerObject.warn(message);
     }
   }
 
-  public error(message: String) {
+  public error(message: string): void {
     if (this.shouldLog(LogLevel.ERROR)) {
       this.loggerObject.error(message);
     }

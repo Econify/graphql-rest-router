d022ac60ac3530fd8b3e59cd3a23e7ac8bf7e37a
Regan Karlewicz <regan@econify.com>
2021-10-11 20:50:45 -0700

    [RK] remove no-explicit-any in Route.ts

--- src/types.ts
+++ src/types.ts
@@ -56,7 +56,7 @@ export interface IOperationVariable {
   required: boolean;
   type: string;
   array: boolean;
-  defaultValue?: string | boolean | number;
+  defaultValue?: string | boolean | number | null;
 }
 
 export interface IResponse {
